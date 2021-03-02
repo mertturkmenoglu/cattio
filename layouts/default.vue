a26866e976458bb13832aac59369dc031d30eb20
Mert <mertturkmenoglu99@gmail.com>
2021-03-02 19:28:49 +0300

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" mini-variant fixed app clipped>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar fixed app flat short :color="appBarColor" clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      items: [
+        {
+          icon: 'mdi-home-roof',
+          title: 'Home',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      title: 'Cattio',
+    }
+  },
+  computed: {
+    appBarColor() {
+      return this.$vuetify.theme.dark ? '#272727' : '#FFF'
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">Cattio</v-card-title>
+        <v-card-text>
+          <p>Testing Nuxt.js with Cattio</p>
+          <hr class="my-3" />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="success" nuxt to="/inspire"> Continue </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {}
+</script>
